1104ae6888caeb0086bb27f6a6a6e9a9fd29150e
morelucks <luckykamshak@gmail.com>
2025-12-23 14:09:52 +0100

    feat(web): wrap app with AppKitProvider

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import { ConnectProvider } from '@stacks/connect-react'
 import App from './App.tsx'
 import './index.css'
+import { AppKitProvider } from './appkit/AppKitProvider'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ConnectProvider>
-      <App />
+      <AppKitProvider>
+        <App />
+      </AppKitProvider>
     </ConnectProvider>
   </React.StrictMode>,
 )
